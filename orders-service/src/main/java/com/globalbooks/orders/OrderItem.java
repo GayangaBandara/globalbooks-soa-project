d0cbbb1e132f32c90c37ce1b113c17815560ad6b
nimesha-thalgaspitiya <gayangabandara74@gmail.com>
2025-08-29 15:35:04 +0530

    Update OrderItem.java

---
+++ orders-service/src/main/java/com/globalbooks/orders/OrderItem.java
@@ -0,0 +1,12 @@
+package com.globalbooks.orders;
+
+public class OrderItem {
+    private String isbn;
+    private int quantity;
+    
+    // Getters and Setters
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
